075c180056fbaa3cbfdac87cd71cd650d9ba523a
faraz <fmmasood@amazon.com>
2023-07-05 09:01:43 +1000

    removed kendra from pom

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -1,4 +1,10 @@
 #/bin/bash
-aws cloudformation delete-stack --stack-name 'SkeletonLambda'
+STACK_NAME='SkeletonLambda'
+echo 'deleting stack: ' $STACK_NAME
+aws cloudformation delete-stack --stack-name $STACK_NAME
+echo 'uploading jar file to s3 bucket'
 aws s3 cp ../target/java11-lambda-skeleton.jar s3://faraz-sydney-bucket/lambda-skeleton/java11-lambda-skeleton.jar
-aws cloudformation deploy --stack-name 'SkeletonLambda' --capabilities CAPABILITY_IAM --template-file lambda-cfn-template.yaml
+echo 'waiting for stack to be deleted'
+sleep 10
+echo 'deploying stack: ' $STACK_NAME
+aws cloudformation deploy --stack-name $STACK_NAME --capabilities CAPABILITY_IAM --template-file lambda-cfn-template.yaml
